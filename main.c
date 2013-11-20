4051fbc5aa46a19aff3f9f5fda15fdd745efdf07
Ryan Hub <c15ryan.hub@usafa.edu>
2013-11-19 20:11:17 -0700

    Made some progress. Got motors to move and figured out how to change their direction. However, motors stop when I place robot on the ground.

--- main.c
+++ main.c
@@ -4,40 +4,37 @@ int main(void)
 {
     WDTCTL = WDTPW|WDTHOLD;                 // stop the watchdog timer
 
-    P1DIR |= BIT1;
-    P1OUT &= ~BIT1;
 
     P1DIR |= BIT2;					// TA0CCR1 on P1.2
-    P1SEL |= BIT2;					// TA0CCR1 on P1.2
-
-
-    //P2DIR |= BIT0;
-    //P2OUT &= ~BIT0;
-
+    P1DIR |= BIT1;
+    P1SEL |= BIT2;
+    P2DIR |= BIT0;
     P2DIR |= BIT1;
-    P2SEL |= BIT1;
+    P2SEL |= BIT0;
+
+    P1OUT |= BIT1;
+    P1OUT &= ~BIT2;
+    P2OUT |= BIT1;
+    P2OUT &= ~BIT0;
 
 
-
-    TACTL &= ~(MC1|MC0);            // stop timer A0
+   TACTL &= ~(MC1|MC0);            // stop timer A0
 
     TACTL |= TACLR;                // clear timer A0
 
     TACTL |= TASSEL1;           // configure for SMCLK
 
+    //TA1CCR0 = 100;
     TACCR0 = 100;                // set signal period to 100 clock cycles (~100 microseconds)
     TACCR1 = 75;                // set duty cycle to 25/100 (25%)
     TACCR2 = 75;
+    //TA1CCR1 = 0;
 
     TACCTL1 |= OUTMOD_7;        // set TACCTL1 to Reset / Set mode
-    TACCTL2 |= OUTMOD_7;
+    //TA0CCTL0 |= OUTMOD_5;
 
     TACTL |= MC0;                // count up
 
-    while (1) {
-
-
-    }
-
+    while (1) {}
     return 0;
 }
