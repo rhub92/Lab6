7443d2db08bd6a956509d901f37af59b860fce58
Ryan Hub <c15ryan.hub@usafa.edu>
2013-11-21 12:44:18 -0700

    Fixed code with Captain B, so it wouldn't reset

--- main.c
+++ main.c
@@ -9,37 +9,37 @@ int main(void)
     while (1) {
 
     	moveRobotForward();
-    	//_delay_cycles(2000000);
+    	_delay_cycles(1500000);
     	stopRobot();
-    	_delay_cycles(2000000);
+    	_delay_cycles(1500000);
 
 
     	moveRobotBackwards();
-    	//_delay_cycles(2000000);
+    	_delay_cycles(1500000);
     	stopRobot();
-    	_delay_cycles(2000000);
+    	_delay_cycles(1500000);
 
 
     	moveRobotSharpLeft();
-    	//_delay_cycles(2000000);
+    	_delay_cycles(1500000);
     	stopRobot();
-    	_delay_cycles(2000000);
+    	_delay_cycles(1500000);
 
 
     	moveRobotSharpRight();
-    	//_delay_cycles(2000000);
+    	_delay_cycles(1500000);
     	stopRobot();
-    	_delay_cycles(2000000);
+    	_delay_cycles(1500000);
 
     	moveRobotLeft();
-    	//_delay_cycles(2000000);
+    	_delay_cycles(1500000);
     	stopRobot();
-    	_delay_cycles(2000000);
+    	_delay_cycles(1500000);
 
     	moveRobotRight();
-    	//_delay_cycles(2000000);
+    	_delay_cycles(1500000);
     	stopRobot();
-    	_delay_cycles(2000000);
+    	_delay_cycles(1500000);
 
     }
     return 0;
--- motorMovement.c
+++ motorMovement.c
@@ -10,14 +10,16 @@
 void initMotorTimer() {
 
     P1DIR |= BIT2;
-    P1DIR |= BIT1;
-    //P1SEL |= BIT2;
-    P2DIR |= BIT0;
-    P2DIR |= BIT1;
-    //P2SEL |= BIT0;
-    //P1SEL |= BIT1;
-    //P2SEL |= BIT1;
+    P1SEL |= BIT2;
 
+    P1DIR |= BIT1;
+    P1SEL |= BIT1;
+
+    P2DIR |= BIT0;
+    P2SEL |= BIT0;
+
+    P2DIR |= BIT1;
+    P2SEL |= BIT1;
 
     TA0CTL &= ~(MC1|MC0);
     TA1CTL &= ~(MC1|MC0);
@@ -33,136 +35,73 @@ void initMotorTimer() {
     TA1CCR0 = 100;
     TA1CCR1 = 0;
 
+    TA0CCTL0 |= OUTMOD_5;
+    TA0CCTL1 |= OUTMOD_7;
+    TA1CCTL0 |= OUTMOD_5;
+    TA1CCTL1 |= OUTMOD_7;
 
     TA0CTL |= MC0;
     TA1CTL |= MC0;
 }
 
 void leftMotorForward() {
-    P1SEL |= BIT1;
-    P1SEL |= BIT2;
-    //P2SEL &= ~BIT0;
-    //P2SEL &= ~BIT1;
-
 	TA0CCTL0 |= OUTMOD_5;
-    TA0CCTL1 |= OUTMOD_7;
-    //TA1CCTL0 |= OUTMOD_7;
-    //TA1CCTL1 |= OUTMOD_5;
-
-    TA0CCR0 = 100;
-    //TA1CCR1 = 0;
+	_delay_cycles(10000);
     TA0CCR1 = 60;
-    //_delay_cycles(5000000);
-    //TA0CCR1 = 0;
 }
 
 void rightMotorForward() {
-    P2SEL |= BIT0;
-    P2SEL |= BIT1;
-    //P1SEL &= ~BIT1;
-    //P1SEL &= ~BIT2;
-
 	TA1CCTL0 |= OUTMOD_5;
-    TA1CCTL1 |= OUTMOD_7;
-
-    TA1CCR0 = 100;
-    //TA0CCR1 = 0;
+	_delay_cycles(10000);
     TA1CCR1 = 60;
-    //_delay_cycles(5000000);
-    //TA1CCR1 = 0;
 }
 
 void leftMotorBackwards() {
-    P1SEL |= BIT1;
-    P1SEL |= BIT2;
-
-	TA0CCTL0 |= OUTMOD_7;
-    TA0CCTL1 |= OUTMOD_7;
-
-    TA0CCR0 = 100;
-    //TA1CCR1 = 0;
-    TA0CCR1 = 40;
+	TA0CCTL0 &= ~OUTMOD0;
+	TA0CCTL0 |= OUTMOD_4;
+	_delay_cycles(10000);
+	TA0CCR1 = 0;
 }
 
 void rightMotorBackwards() {
-    P2SEL |= BIT0;
-    P2SEL |= BIT1;
-
-	TA1CCTL0 |= OUTMOD_7;
-    TA1CCTL1 |= OUTMOD_7;
-
-    TA1CCR0 = 100;
-    //TA0CCR1 = 0;
-    TA1CCR1 = 40;
+	TA1CCTL0 &= ~OUTMOD0;
+	TA1CCTL0 |= OUTMOD_4;
+	_delay_cycles(10000);
+	TA1CCR1 = 0;
 }
 
 void moveRobotForward() {
 	leftMotorForward();
 	rightMotorForward();
-	_delay_cycles(2000000);
-	TA0CCTL0 &= ~OUTMOD_7;
-    TA0CCTL1 &= ~OUTMOD_7;
-	TA1CCTL0 &= ~OUTMOD_7;
-    TA1CCTL1 &= ~OUTMOD_7;
 }
 
 void moveRobotBackwards() {
 	leftMotorBackwards();
 	rightMotorBackwards();
-	_delay_cycles(2000000);
-	TA0CCTL0 &= ~OUTMOD_7;
-    TA0CCTL1 &= ~OUTMOD_7;
-	TA1CCTL0 &= ~OUTMOD_7;
-    TA1CCTL1 &= ~OUTMOD_7;
 }
 
 void stopRobot() {
-    P2SEL &= ~BIT0;
-    P2SEL &= ~BIT1;
-    P1SEL &= ~BIT1;
-    P1SEL &= ~BIT2;
+	TA0CCTL0 |= OUTMOD_5;
+	TA1CCTL0 |= OUTMOD_5;
 
-    TA0CCR0 = 100;
-    TA1CCR0 = 100;
     TA0CCR1 = 0;
     TA1CCR1 = 0;
-
 }
 
 void moveRobotSharpLeft() {
 	rightMotorForward();
 	leftMotorBackwards();
-	_delay_cycles(2000000);
-	TA0CCTL0 &= ~OUTMOD_7;
-    TA0CCTL1 &= ~OUTMOD_7;
-	TA1CCTL0 &= ~OUTMOD_7;
-    TA1CCTL1 &= ~OUTMOD_7;
 }
 
 void moveRobotSharpRight() {
 	leftMotorForward();
 	rightMotorBackwards();
-	_delay_cycles(2000000);
-	TA0CCTL0 &= ~OUTMOD_7;
-    TA0CCTL1 &= ~OUTMOD_7;
-	TA1CCTL0 &= ~OUTMOD_7;
-    TA1CCTL1 &= ~OUTMOD_7;
 }
 
 void moveRobotLeft() {
 	rightMotorForward();
-	_delay_cycles(2000000);
-	TA0CCTL0 &= ~OUTMOD_7;
-    TA0CCTL1 &= ~OUTMOD_7;
-	TA1CCTL0 &= ~OUTMOD_7;
-    TA1CCTL1 &= ~OUTMOD_7;
 }
 
 void moveRobotRight() {
 	leftMotorForward();
-	_delay_cycles(2000000);
-	TA0CCTL0 &= ~OUTMOD_7;
-    TA0CCTL1 &= ~OUTMOD_7;
-	TA1CCTL0 &= ~OUTMOD_7;
-    TA1CCTL1 &= ~OUTMOD_7;
 }
